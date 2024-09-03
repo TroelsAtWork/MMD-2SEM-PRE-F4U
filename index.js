bee2c5f8504026200028e42b0acf9fd599c70137
TroelsAtWork <tras@kea.dk>
2024-09-03 12:01:28 +0200

    Add product image

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const categoryList = document.querySelector("#categoryList");
-
-fetch("https://kea-alt-del.dk/t7/api/categories")
-  .then((response) => response.json())
-  .then((categories) => {
-    categories.forEach((category) => {
-      categoryList.innerHTML += `<li><a href="?category=${category.category}"=>${category.category}</a></li>`;
-    });
-  });
-
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate").content;
-const params = new URLSearchParams(document.location.search);
-let url = undefined;
-
-if (params.has("category")) {
-  url = `https://kea-alt-del.dk/t7/api/products?category=${params.get("category")}`;
-} else {
-  url = "https://kea-alt-del.dk/t7/api/products";
-}
 
 function duplicateTemplate(template, container) {
-  fetch(url)
+  fetch("https://kea-alt-del.dk/t7/api/products")
     .then((response) => response.json())
     .then((data) => {
       data.forEach((product) => {
@@ -30,9 +12,9 @@ function duplicateTemplate(template, container) {
         templateClone.querySelector(".price").textContent = product.price;
         templateClone.querySelector(".brand").textContent = product.brandname;
         templateClone.querySelector("#seemore").setAttribute("href", `details.html?productid=${product.id}`);
-        if (product.soldout) {
-          templateClone.querySelector("#soldoutLabel").classList.add("soldout");
-        }
+        templateClone
+          .querySelector("img")
+          .setAttribute("src", `https://kea-alt-del.dk/t7/images/jpg/640/${product.id}.jpg`);
 
         container.appendChild(templateClone);
       });
