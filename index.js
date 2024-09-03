e67da022f8793ddd801c402d206c57f3c922968c
TroelsAtWork <tras@kea.dk>
2024-09-03 15:03:46 +0200

    Add conditional rendering to productlist

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const productContainer = document.querySelector("#productContainer");
 const productTemplate = document.querySelector("#productTemplate").content;
+const categoryList = document.querySelector("#categoryList");
+const params = new URLSearchParams(document.location.search);
+const category = params.get("category");
+let url = undefined;
+
+fetch("https://kea-alt-del.dk/t7/api/categories")
+  .then((response) => response.json())
+  .then((categories) => {
+    categories.forEach((category) => {
+      categoryList.innerHTML += `<li><a href="index.html?category=${category.category}">${category.category}</a></li>`;
+    });
+  });
+
+if (params.has("category")) {
+  url = "https://kea-alt-del.dk/t7/api/products?category=" + category;
+} else {
+  url = "https://kea-alt-del.dk/t7/api/products";
+}
 
 function duplicateTemplate(template, container) {
-  fetch("https://kea-alt-del.dk/t7/api/products")
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
       data.forEach((product) => {
